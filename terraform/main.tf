ecd785283aa2ca29a86215cfd792e219548c6a35
chaitanya717 <chaudharichaitanya998@gmail.com>
2024-09-11 16:28:36 +0530

    sd

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,57 +27,57 @@ module "iam-profile" {
   iam-instance-role-policy-name = var.iam-instance-role-policy-name
 }
 
-# module "ec2_servers" {
-# source = "./module/launchtemplate"
-# iam_instance_profile_name = module.iam-profile.iam_instance_profile_name
-# region = var.region
-# key_name = var.key_name
-# public_key = local.db_Creds.publickey
+module "ec2_servers" {
+source = "./module/launchtemplate"
+iam_instance_profile_name = module.iam-profile.iam_instance_profile_name
+region = var.region
+key_name = var.key_name
+public_key = local.db_Creds.publickey
 
-# launch_template_name_presentetion_tier = var.launch_template_name_presentetion_tier
-# instance_type_presentetion_tier = var.instance_type_presentetion_tier
-# key_name_presentation_tier = var.key_name
-# security_groups_presentetion_tier = [module.security_groups.presentation_tier_sg_id]
-# load_balancer_presentetion_tier = module.loab_balancer.presentation_alb
-# ecr_url = local.db_Creds.ecr_url
-# ecr_repo_name_presentetion_tier = local.db_Creds.frontend_repo_ecr
+launch_template_name_presentetion_tier = var.launch_template_name_presentetion_tier
+instance_type_presentetion_tier = var.instance_type_presentetion_tier
+key_name_presentation_tier = var.key_name
+security_groups_presentetion_tier = [module.security_groups.presentation_tier_sg_id]
+load_balancer_presentetion_tier = module.loab_balancer.presentation_alb
+ecr_url = local.db_Creds.ecr_url
+ecr_repo_name_presentetion_tier = local.db_Creds.frontend_repo_ecr
 
-# #  application tier
-# db_name = local.db_Creds.rdsdb 
-# rds_db_admin =  local.db_Creds.rdsusername
-# rds_db_password = local.db_Creds.rdspassword
-# rds_db_adresss = module.rds_instance.rds_instance_hostname
+#  application tier
+db_name = local.db_Creds.rdsdb 
+rds_db_admin =  local.db_Creds.rdsusername
+rds_db_password = local.db_Creds.rdspassword
+rds_db_adresss = module.rds_instance.rds_instance_hostname
 
-# multi_az = var.multi_az
-# engine_version = var.engine_version
-# db_engine = var.db_engine
-# instance_class = var.instance_class
-# allocated_storage = var.allocated_storage
-# key_name_application_tier = var.key_name
-# load_balancer_application_tier = module.loab_balancer.application_alb
-# launch_template_name_application_tier = var.launch_template_name_application_tier
-# instance_type_application_tier = var.instance_type_application_tier
-# ecr_repo_name_application_tier = local.db_Creds.backend_repo_ecr
-# security_groups_application_tier = [module.security_groups.application_tier_sg_id]
-# nat_gateway = module.vpc.natgetway
-# }
+multi_az = var.multi_az
+engine_version = var.engine_version
+db_engine = var.db_engine
+instance_class = var.instance_class
+allocated_storage = var.allocated_storage
+key_name_application_tier = var.key_name
+load_balancer_application_tier = module.loab_balancer.application_alb
+launch_template_name_application_tier = var.launch_template_name_application_tier
+instance_type_application_tier = var.instance_type_application_tier
+ecr_repo_name_application_tier = local.db_Creds.backend_repo_ecr
+security_groups_application_tier = [module.security_groups.application_tier_sg_id]
+nat_gateway = module.vpc.natgetway
+}
 
-# module "loab_balancer" {
-# source = "./module/Alb"
-# vpc_id = module.vpc.vpc_id
+module "loab_balancer" {
+source = "./module/Alb"
+vpc_id = module.vpc.vpc_id
 
-# alb_presentation_tier_name = var.alb_presentation_tier_name
-# alb_presentation_tier_load_balancer_type = var.alb_presentation_tier_load_balancer_type
-# alb_presentation_tier_security_groups = [module.security_groups.alb_presentation_tier_sg_id]
-# alb_presentation_tier_subnets = module.vpc.public_subnet.*.id
-# alb_presentation_tier_enable_deletion_protection = var.alb_presentation_tier_enable_deletion_protection
+alb_presentation_tier_name = var.alb_presentation_tier_name
+alb_presentation_tier_load_balancer_type = var.alb_presentation_tier_load_balancer_type
+alb_presentation_tier_security_groups = [module.security_groups.alb_presentation_tier_sg_id]
+alb_presentation_tier_subnets = module.vpc.public_subnet.*.id
+alb_presentation_tier_enable_deletion_protection = var.alb_presentation_tier_enable_deletion_protection
 
-# alb_application_tier_name = var.alb_application_tier_name
-# alb_application_tier_load_balancer_type = var.alb_application_tier_load_balancer_type
-# alb_application_tier_security_groups = [module.security_groups.alb_application_tier_sg_id]
-# alb_application_tier_subnets = module.vpc.private_subnet.*.id
-# alb_application_tier_enable_deletion_protection = var.alb_presentation_tier_enable_deletion_protection
-# }
+alb_application_tier_name = var.alb_application_tier_name
+alb_application_tier_load_balancer_type = var.alb_application_tier_load_balancer_type
+alb_application_tier_security_groups = [module.security_groups.alb_application_tier_sg_id]
+alb_application_tier_subnets = module.vpc.private_subnet.*.id
+alb_application_tier_enable_deletion_protection = var.alb_presentation_tier_enable_deletion_protection
+}
 
 # module "rds_instance" {
 #   source = "./module/Rds"
